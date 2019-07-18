239c73b57609f79e0c66138400c179447f5173bc
Srijith Umakanth <srijithumakanth@gmail.com>
2019-07-18 22:32:09 +0000

    Correct path for world file

--- shellScripts/wall_follower.sh
+++ shellScripts/wall_follower.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/catkin_ws/src/world/myworld.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/myworld.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file="/home/workspace/catkin_ws/src/slam_gmapping/gmapping/launch/slam_gmapping_pr2.launch" " &
 sleep 5
