43d29c764e354e7eebb9c15893df8a82e7ca7a48
Srijith Umakanth <srijithumakanth@gmail.com>
2019-07-18 21:50:37 +0000

    Initial commit

---
+++ catkin_ws/src/shellScripts/test_slam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/catkin_ws/src/world/myworld.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 7
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"&
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
