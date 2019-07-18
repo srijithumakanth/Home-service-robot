8c4baa700455e9bafb161a888b3742854aa83857
Srijith Umakanth <srijithumakanth@gmail.com>
2019-07-18 22:34:44 +0000

    increased time for wall follower node

--- shellScripts/wall_follower.sh
+++ shellScripts/wall_follower.sh
@@ -4,5 +4,5 @@ sleep 7
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file="/home/workspace/catkin_ws/src/slam_gmapping/gmapping/launch/slam_gmapping_pr2.launch" " &
 sleep 5
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"&
-sleep 5
+sleep 10
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; rosrun wall_follower wall_follower"
