30fb056f9a8ae16f08648f279fcc9da6522995f2
Srijith Umakanth <srijithumakanth@gmail.com>
2019-07-19 14:23:14 +0000

    Shell script to test navigation

---
+++ shellScripts/test_navigation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/myworld.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 7
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/myworld.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
