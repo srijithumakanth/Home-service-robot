5cf2ad8dfa53b455178cce7f40de581326de026f
Srijith Umakanth <srijithumakanth@gmail.com>
2019-07-19 14:52:34 +0000

    To launch pick_objects node

--- shellScripts/test_navigation.sh
+++ shellScripts/test_navigation.sh
@@ -3,4 +3,6 @@ xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GA
 sleep 7
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/myworld.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects"
