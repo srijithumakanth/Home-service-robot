06fdad07634893167eb344f37c8eed7f77036d18
Srijith Umakanth <srijithumakanth@gmail.com>
2019-07-19 14:13:02 +0000

    Updated path

--- shellScripts/wall_follower.sh
+++ shellScripts/wall_follower.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/myworld.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:="/home/workspace/catkin_ws/src/home_service_robot/launch/slam_config.launch" " &
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"&
 sleep 10
