92498c5e860176b1859ce040fe3e0621ac7b6838
Srijith Umakanth <SrijithUmakanth@gmail.com>
2019-07-20 01:54:06 +0000

    Final launch

---
+++ shellScripts/home_service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/myworld.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 7
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="/home/workspace/catkin_ws/src/home_service_robot/world/my_world.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; rosrun add_markers home_service" &
+sleep 2
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; rosrun pick_objects pick_objects"
